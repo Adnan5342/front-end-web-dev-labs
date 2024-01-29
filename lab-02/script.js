1fa696c289dcdca9d06cb6391bbcd5fbdb4ce6b4
P2674500 <P2674500@my365.dmu.ac.uk>
2024-01-29 14:35:30 +0000

    lab-02: created a new web page and experimented with more element styling and buttons

---
+++ lab-02/script.js
@@ -0,0 +1,9 @@
+<script src="scripts.js"></script>
+
+function greetMe() {
+    console.log("Hello");
+}
+
+<p>
+    Clicking <button id="myTrigger">here</button> should trigger the event handler.
+</p>
