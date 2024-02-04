24e078a0adc47d862186920a805ee56742bc7673
P2674500 <P2674500@my365.dmu.ac.uk>
2024-02-04 18:04:09 +0000

    Lab 02: added event handler to a button

--- lab-02/script.js
+++
@@ -1,9 +0,0 @@
-<script src="scripts.js"></script>
-
-function greetMe() {
-    console.log("Hello");
-}
-
-<p>
-    Clicking <button id="myTrigger">here</button> should trigger the event handler.
-</p>
